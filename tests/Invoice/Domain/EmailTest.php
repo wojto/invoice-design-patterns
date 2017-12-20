c85cd41f0271593875e5dcc46f3b784087e72f5a
Wojciech Andruszkiewicz <wandruszkiewicz@melog.com>
2017-12-20 13:09:39 +0100

    unit tests run-script.php

---
+++ tests/Invoice/Domain/EmailTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wojto
+ * Date: 20.12.17
+ * Time: 13:01
+ */
+
+namespace tests\Invoice\Domain;
+
+
+use Invoice\Domain\Email;
+use PHPUnit\Framework\TestCase;
+
+class EmailTest extends TestCase
+{
+    /**
+     * @param string $email
+     * @dataProvider invalidEmails
+     */
+    public function testThatCannotBeCreatedFromInvalidEmail(string $email)
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        new Email($email);
+    }
+
+    public function invalidEmails(): array
+    {
+        return [
+            ['invalid-email'],
+            [''],
+            ['codesensus@'],
+            ['codesensus@gmail']
+        ];
+    }
+}
